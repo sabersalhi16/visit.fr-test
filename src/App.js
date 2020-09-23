2f2756d5d9b3f99f0e0bb8729e13b798ec56e4a7
Saber <sabersalhi16@gmail.com>
2020-09-23 13:27:25 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect } from "react";
 
+import "./App.css";
+const CLIENT_ID = process.env.CLIENT_ID;
 function App() {
+  useEffect(() => {
+    window.gapi.load("client:auth2", function () {
+      window.gapi.auth2.init({
+        client_id: CLIENT_ID,
+      });
+    });
+  }, []);
+  function execute() {
+    return window.gapi.client.pagespeedonline.pagespeedapi
+      .runpagespeed({
+        category: ["performance"],
+        strategy: "mobile",
+        url: "https://www.voici.fr/",
+      })
+      .then(
+        function (response) {
+          // Handle the results here (response.result has the parsed body).
+          console.log("Response", response);
+        },
+        function (err) {
+          console.error("Execute error", err);
+        }
+      );
+  }
+  function loadClient() {
+    window.gapi.client.setApiKey("AIzaSyDzBl7JJLD1D0GFh_RXsqwHg4rKUJJZz8k");
+    return window.gapi.client
+      .load(
+        "https://content.googleapis.com/discovery/v1/apis/pagespeedonline/v5/rest"
+      )
+      .then(
+        function () {
+          console.log("GAPI client loaded for API");
+        },
+        function (err) {
+          console.error("Error loading GAPI client for API", err);
+        }
+      );
+  }
+  function authenticate() {
+    return window.gapi.auth2
+      .getAuthInstance()
+      .signIn({ scope: "openid" })
+      .then(
+        function () {
+          console.log("Sign-in successful");
+        },
+        function (err) {
+          console.error("Error signing in", err);
+        }
+      );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={() => authenticate().then(loadClient)}>
+        authorize and load
+      </button>
+      <button onClick={() => execute}>execute</button>
     </div>
   );
 }
